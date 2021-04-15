8b53a839928ceae3b321a33d726a5df9531d7f58
nivriii <nivri.learning@gmail.com>
2021-04-14 22:12:46 -0500

    Tipos de Scope

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,5 @@
-function MediaPlayer(config) {
-    this.media = config.element;
-}
+import MediaPlayer from './MediaPlayer.js';
 
-MediaPlayer.prototype.play = function() {
-    this.media.play();
-};
-
-MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-};
-
-MediaPlayer.prototype.togglePlay = function() {
-    this.media.paused ? this.play() : this.pause();
-};
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 const player = new MediaPlayer({ element: video });
