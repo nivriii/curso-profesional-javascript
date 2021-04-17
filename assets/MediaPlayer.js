0d21a31adfc8b007301ff5a83cea168ebea14756
nivriii <nivri.learning@gmail.com>
2021-04-16 20:48:53 -0500

    El primer plugin autoplay y mute button

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config) {
     this.media = config.element;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
 
 MediaPlayer.prototype.play = function() {
     this.media.play();
@@ -12,6 +21,18 @@ MediaPlayer.prototype.pause = function() {
 
 MediaPlayer.prototype.togglePlay = function() {
     this.media.paused ? this.play() : this.pause();
+};
+
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleMute = function() {
+    this.media.muted = !this.media.muted;
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
-const button = document.querySelector("button");
-const player = new MediaPlayer({ element: video });
+const buttonPlay = document.getElementById("togglePlay");
+const buttonMute = document.getElementById("toggleMute");
+const player = new MediaPlayer({ element: video, plugins: [ new AutoPlay() ] });
 
-button.onclick = () => player.togglePlay();
+buttonPlay.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMute();
