77dbab70a4938c81fdaa76bdc8b3417cd80d608c
nivriii <nivri.learning@gmail.com>
2021-04-17 14:10:51 -0500

    Concepto this

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,12 @@ import AutoPlay from "./plugins/AutoPlay.js";
 const video = document.querySelector("video");
 const buttonPlay = document.getElementById("togglePlay");
 const buttonMute = document.getElementById("toggleMute");
-const player = new MediaPlayer({ element: video, plugins: [ new AutoPlay() ] });
+const player = new MediaPlayer({ 
+    element: video,
+    plugins: [
+        // new AutoPlay()
+    ]
+});
 
 buttonPlay.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
